b4f8a9eaa1e35b7779756e644defe7fdb461ca0d
SouravM9 <majumdars598@gmail.com>
2023-05-03 16:12:16 +0530

    Card edited for spacing

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
-
 function Card(props) {
     return (
         <div className="card m-3" style={{ width: '13rem' }}>
-            <img src={props.image} className="card-img-top" alt="alt" style={{ width: 'auto', height: '230px' }} />
+            <img src={props.image} className="card-img-top" alt="alt" style={{ height: '230px', objectFit: 'cover' }} />
             <div className="card-body">
-                <h5 className="card-title text-center">{props.title}</h5>
-                <p className="card-text text-center">{props.desc}</p>
-                <button type="button" className="btn btn-primary" onClick={(e) => { props.handleOnEditClick(props.id) }}>Edit</button>
-                <button type="button" className="btn btn-danger m-2" style={{ right: '0px' }} onClick={() => props.handleDelete(props.id)}>Delete</button>
+                <h5 className="card-title text-center mb-3">{props.title}</h5>
+                <p className="card-text text-center mb-4">{props.desc}</p>
+                <div className="d-flex justify-content-center">
+                    <button type="button" className="btn btn-primary me-2" onClick={(e) => { props.handleOnEditClick(props.id) }}>Edit</button>
+                    <button type="button" className="btn btn-danger" onClick={() => props.handleDelete(props.id)}>Delete</button>
+                </div>
             </div>
         </div>
     )
