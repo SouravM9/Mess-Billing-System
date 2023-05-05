0fa614482fb71a835c4039a8afef1f74d96c0047
SouravM9 <majumdars598@gmail.com>
2023-05-05 16:29:35 +0530

    Production Ready Check-in

--- index.js
+++ index.js
@@ -39,13 +39,12 @@ app.use('/meals', mealRouter);
 
 // Production condition
 
-// if (process.env.NODE_ENV == "production") {
-//     app.use(express.static('client/build'))
-//     const path = require('path')
-//     app.get("*", (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    //*Set static folder up in production
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+  }
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
